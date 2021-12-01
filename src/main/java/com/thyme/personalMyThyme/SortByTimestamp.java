cc5926c049a0e5b400c6349e182483fc5e21f3f9
Kamrun Foster <kamrun.foster4@gmail.com>
2021-11-30 20:54:53 -0600

    working on adding to new project

---
+++ src/main/java/com/thyme/personalMyThyme/SortByTimestamp.java
@@ -0,0 +1,13 @@
+//package com.thyme.personalMyThyme;
+//
+//import com.thyme.mythyme.models.Messages;
+//
+//import java.util.Comparator;
+//
+//public class SortByTimestamp implements Compartor<Messages> {
+//    public int compare(Messages o1, Messages o2){
+//        if (o1.getTimestamp() == null || o2.getTimestamp() == null)
+//            return 0;
+//        return o1.getTimestamp().compareTo(o2.getTimestamp());
+//    }
+//}
